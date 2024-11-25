f664b59dc95940a05147c3b92cd5aae3924693c5
niloferh <nilofer.hyder@mail.utoronto.ca>
2024-11-24 20:11:22 -0500

    SCRUM-70 Welcome page for patients

--- userserver/urls.py
+++ userserver/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('users/get_clinician_info', views.get_clinician_info),
-    path('users/find_all_patients', views.find_all_patients)
+    path('users/find_all_patients', views.find_all_patients),
+    path('users/get_patient_info', views.get_patient_info)
 ]
--- userserver/views.py
+++ userserver/views.py
@@ -29,3 +29,15 @@ def get_clinician_info(request):
             return JsonResponse({"message": "Clinician with that email does not exist"}, status=404)
     except Exception as e:
         return JsonResponse({"message": str(e)}, status=500)
+    
+@api_view(['GET'])
+def get_patient_info(request):
+    params = request.query_params
+    try:
+        obj = Patients.objects.filter(email=params['email']).all()
+        if (obj is not None):
+            return JsonResponse({"message": list(obj.values())[0]}, status=200)
+        else:
+            return JsonResponse({"message": "Patient with that email does not exist"}, status=404)
+    except Exception as e:
+        return JsonResponse({"message": str(e)}, status=500)
