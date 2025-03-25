e8d3ee3085250866f1bc42ea8fd5aabe7be77a1d
zoya5636 <zoya@chishtie.com>
2025-03-24 21:08:34 -0400

    small fixes

--- userserver/urls.py
+++ userserver/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('users/get_clinician_info', views.get_clinician_info),
-    path('users/find_all_patients', views.find_all_patients),
+    path('users/get_all_patients', views.get_all_patients),
     path('users/get_patient_info', views.get_patient_info),
     path('users/add_clinician', views.add_clinician),
     path('users/check_if_clinician_exists', views.check_if_clinician_exists),
